b83fa958c5ec53153ece57073c9913a5c5983c87
ViktoriiaKrasko <akiv.co@gmail.com>
2023-08-22 18:23:52 +0300

    event

--- src/container/3/index.js
+++ src/container/3/index.js
@@ -4,22 +4,37 @@ const button = document.querySelector('.form__button')
 document.addEventListener('fullscreenchange', (e) => {
   console.log(e)
 
-  alert(
-    `Повноекраний режим ${
-      document.fullscreenElement !== null
-        ? 'увімкнуто'
-        : 'вимкнуто'
-    }.`,
-  )
+  // alert(
+  //   `Повноекраний режим ${
+  //     document.fullscreenElement !== null
+  //       ? 'увімкнуто'
+  //       : 'вимкнуто'
+  //   }.`,
+  // )
 })
 
-// Обробник події "fullscreenerror"
-document.addEventListener('fullscreenerror', (e) => {
-  console.log(e)
-  alert(
-    'Помилка при спробі перейти до повноекранного режиму.',
-  )
+//Обробник натискання на кнопку повноекранного режиму
+button.addEventListener('click', () => {
+  if (document.fullscreenElement) {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    }
+  }
+
+  const body = document.documentElement
+  if (body.requestFullscreen) {
+    body.requestFullscreen()
+  } else {
+    alert('Повноекранний режим не підтримується')
+  }
 })
+// Обробник події "fullscreenerror"
+// document.addEventListener('fullscreenerror', (e) => {
+//   console.log(e)
+//   alert(
+//     'Помилка при спробі перейти до повноекранного режиму.',
+//   )
+// })
 
 // Обробник натискання на кнопку повноекранного режиму
 button.addEventListener('click', () => {
